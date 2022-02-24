dac0f95450bfcc31c420c2ff274a0ce2d799dd1b
AndreasZi <andiziegl@web.de>
2022-02-25 00:43:52 +0100

    js fetch instead of <object>

--- src/main.cpp
+++ src/main.cpp
@@ -12,8 +12,7 @@
 #include <ESPAsyncWebServer.h>
 
 #include "secrets.h" 
-/*
-*/
+
 
 
 #define SENSOR_PIN1 A3
@@ -30,8 +29,8 @@ const char* PARAM_MESSAGE = "message";
 
 int sensorValue = 0;
 
-const String htmlPage = "<h3>Sensor 1: </h3><object id='value' data='sensorValue' height='45' width='80'></object><script onload='clearInterval(myVar)'>var obj = document.getElementById('value');let myVar = setInterval(myTimer, 5000);function myTimer(){obj.data=obj.data;};</script>";
-
+const String htmlPage =  "<script>async function request (){const response = await fetch('http://192.168.2.160/sensorValue');return response.json()} function something (){request().then(data=>{document.getElementById('sensorValue').innerHTML=data.brightness;setTimeout(something,1000)});};  </script><button onclick='something()'>Der testbutton</button><h3>Sensoren Raum 1: </h3><p>Brightness <a id='sensorValue'></a></p>";
+//"<h3>Sensoren Raum 1: </h3><p>Brightness <object id='value' data='sensorValue' height='40' width='80'></object></p><script onload='clearInterval(intervall)'>var obj = document.getElementById('value');let intervall = setInterval(reloadObj, 2500);function reloadObj(){obj.data=obj.data;};</script>";
 
 void notFound(AsyncWebServerRequest *request) {
     request->send(404, "text/plain", "Not found");
@@ -58,7 +57,7 @@ void setup() {
     });
 
     server.on("/sensorValue", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(200, "text/html", "<p>" + String(sensorValue) + "</p>");
+        request->send(200, "text/plain", "{\"brightness\": \"" + String(sensorValue) + " %\"}");
     });
    
 
@@ -68,6 +67,6 @@ void setup() {
 }
 
 void loop() {
-sensorValue = analogRead(SENSOR_PIN1);
+sensorValue = 100*(4096 - analogRead(SENSOR_PIN1))/4096;
 delay(1000);
 }
