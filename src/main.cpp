e82e22b9a36dd09ea1a4c20722e9cf2de5576f68
AndreasZi <andiziegl@web.de>
2022-02-27 00:09:43 +0100

    Now Add variables to updating cycle instead of reading directliy, should make interface more versatile

--- src/main.cpp
+++ src/main.cpp
@@ -6,13 +6,21 @@
 //
 
 #include "webinterface.h"
-
+int wert = 0;
+int status = 0;
 
 void setup() {
     startWebinterface();
+    addVariable(wert, "timer");
+    addVariable(status, "Status: ");
+    index_html = 
+        "<style>div{width:100%; diplay: flex;float:left;}div.variable{border-style: inset;width: 8em; padding: 0.8em;}</style>"
+        "<h1>Lieblings-ESP</h1>"
+        "<div><h2>Variablen: </h2></div>"
+        "<div>"+VARIABLES+"</div>";
 }
 
 void loop() {
     wert ++;
-    delay(1000);
+    delay(800);
 }
--- src/webinterface.h
+++ src/webinterface.h
@@ -2,55 +2,79 @@
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+
 #include "secrets.h" 
 
-int wert = 0;
+#define REFRESH_INTERVALL "500"
+#define CLASSNAME "variable"
+#define VARIABLE_SPACE 10
+
+String index_html;
+String VARIABLES = "<script onload='clearInterval(myInterval);'>const variables=[];let myInterval=setInterval(changeValues, "REFRESH_INTERVALL");async function request (){const response = await fetch('data');return response.json()};function changeValues (){request().then(data=>{dt=data;for(let x in variables){document.getElementById(variables[x]).innerHTML=dt[variables[x]];};});};</script>";
 
 
-AsyncWebServer server(80);
 
-String html = "<script onload='clearInterval(myInterval);'>const sensors=[];let myInterval=setInterval(changeValues, 1000);async function request (){const response = await fetch('data');return response.json()};function changeValues (){request().then(data=>{dt=data;for(let x in sensors){document.getElementById(sensors[x]).innerHTML=dt[sensors[x]];};});};</script>";
+int counter = 0;
+String names[VARIABLE_SPACE];
 
-class liveData{
-    public:
-    int* value;
-    String name;
-    liveData(int &VALUE, String NAME){
-        value = &VALUE;
-        name = NAME;
-        html += "<div class='sensor'>"+name+" <a id='"+name+"'>-</a></div><script>sensors.push('"+name+"'); </script>";
-    }
-};
 
-liveData linked_values[1]={liveData(wert,"name")};
+int* integers[VARIABLE_SPACE];
+float* floats[VARIABLE_SPACE];
+String* strings[VARIABLE_SPACE];
+
+void addVariable(int &VALUE, String NAME){ 
+    integers[counter] = &VALUE;
+    names[counter] = NAME;
+    VARIABLES += "<div class='"CLASSNAME"'>"+NAME+" <a id='"+NAME+"'>-</a></div><script>variables.push('"+NAME+"'); </script>";
+    counter++;
+    index_html = VARIABLES;
+}
+void addVariable(float &VALUE, String NAME){
+    floats[counter] = &VALUE;
+    names[counter] = NAME;
+    VARIABLES += "<div class='"CLASSNAME"'>"+NAME+" <a id='"+NAME+"'>-</a></div><script>variables.push('"+NAME+"'); </script>";
+    counter++;
+    index_html = VARIABLES;
+}
+void addVariable(String &VALUE, String NAME){
+    strings[counter] = &VALUE;
+    names[counter] = NAME;
+    VARIABLES += "<div class='"CLASSNAME"'>"+NAME+" <a id='"+NAME+"'>-</a></div><script>variables.push('"+NAME+"'); </script>";
+    counter++;
+    index_html = VARIABLES;
+}
+
 
 String json(){
         String json = "{";
-        for(int i=0; i<1; i++){
+        for(int i=0; i<counter; i++){
             if (i != 0) json += ", ";
-            json += "\"" + linked_values[i].name + "\": \"" + String(*linked_values[i].value)  + "\"";
+            if (integers[i]){
+                json += "\"" + names[i] + "\": " + String(*integers[i]);
+            }
+            if (floats[i]){
+                json += "\"" + names[i] + "\": " + String(*floats[i]);
+            }
+            if (strings[i]){
+                json += "\"" + names[i] + "\": \"" + String(*strings[i])  + "\"";
+            }
         }
         json += "}";
         return json;
     }
 
+AsyncWebServer server(80);
 
-void startWebinterface(){
-    Serial.begin(9600);
+bool startWebinterface(){
 
     WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.printf("Webinterface Failed!\n");
-        return;
+        return false;
     }
 
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());
-    
-
     server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(200, "text/html", html);
+        request->send(200, "text/html", index_html);
     });
 
     server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request){
@@ -58,4 +82,10 @@ void startWebinterface(){
     });
 
     server.begin();
+    
+    Serial.begin(9600);
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    return true;
 }
