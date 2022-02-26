bfff51a993cb049633fbabcee865c3889879389d
AndreasZi <andiziegl@web.de>
2022-02-26 18:47:17 +0100

    moved to header file

--- src/main.cpp
+++ src/main.cpp
@@ -5,74 +5,11 @@
 //  * handle missing pages / 404s
 //
 
-#include <Arduino.h>
-#include <WiFi.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-
 #include "sensor.h"
 
-#include "secrets.h" 
-
-
-//define namie of components 
-#define DEVICE_NAME "Device 1"
-Sensor sensors[2] = {
-    Sensor(A1, "Brightness", "%", 100, -0.0244), //Photeresistior at A1
-    Sensor(D9, "Door")
-    }; 
-
-
-
-
-
-//css, js and html separated for easier editing
-const String index_html = 
-    "<style>div{width:100%; diplay: flex;float:left;}div.sensor{border-style: inset;width: 8em; padding: 0.8em;}</style>"
-    "<script onload='clearInterval(myInterval);'>const sensors=[];let myInterval=setInterval(changeValues, 1000);async function request (){const response = await fetch('data');return response.json()} function changeValues (){request().then(data=>{dt=data;for(let x in sensors){document.getElementById(sensors[x]).innerHTML=dt[sensors[x]];};});};</script>"
-    "<h1>"DEVICE_NAME"</h1>"
-    "<div><h2>Sensoren</h2></div>"
-    "<div>"+sensorhtml+"</div>";
-    ;
-    
-AsyncWebServer server(80);
-
-void notFound(AsyncWebServerRequest *request) {
-    request->send(404, "text/plain", "Not found");
-}
 
 void setup() {
-    Serial.begin(9600);
-    sensors[1].pullup();
-
-    WiFi.mode(WIFI_STA);
-    WiFi.begin(ssid, password);
-    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-        Serial.printf("WiFi Failed!\n");
-        return;
-    }
-
-    Serial.print("IP Address: ");
-    Serial.println(WiFi.localIP());
-
-    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-        request->send(200, "text/html", index_html);
-    });
-
-    server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request){
-        String json = "{";
-        for(int i=0; i<sizeof(sensors)/sizeof(*sensors); i++){
-            if (i != 0) json += ", ";
-            json += "\"" + sensors[i].name + "\": \"" + sensors[i].value  + "\"";
-        }
-        json += "}";
-        request->send(200, "text/plain", json);
-    });
-   
-
-    server.onNotFound(notFound);
-
-    server.begin();
+    startWebinterface();
 }
 
 void loop() {
--- src/sensor.h
+++ src/sensor.h
@@ -1,6 +1,14 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include "secrets.h" 
 
-String sensorhtml = "";
+
+//define namie of components 
+#define DEVICE_NAME "Device 1"
+AsyncWebServer server(80);
+String sensorhtml = "<script onload='clearInterval(myInterval);'>const sensors=[];let myInterval=setInterval(changeValues, 1000);async function request (){const response = await fetch('data');return response.json()} function changeValues (){request().then(data=>{dt=data;for(let x in sensors){document.getElementById(sensors[x]).innerHTML=dt[sensors[x]];};});};</script>";
 
 class Sensor{
     public:
@@ -39,3 +47,55 @@ class Sensor{
         value =  String(factor*analogRead(pin) + offset, 0) + unit;
     }
 };
+
+
+Sensor sensors[2] = {
+    Sensor(A1, "Brightness", "%", 100, -0.0244), //Photeresistior at A1
+    Sensor(D9, "Door")
+    }; 
+
+
+//css, js and html separated for easier editing
+const String index_html = 
+    "<style>div{width:100%; diplay: flex;float:left;}div.sensor{border-style: inset;width: 8em; padding: 0.8em;}</style>"
+    "<h1>"DEVICE_NAME"</h1>"
+    "<div><h2>Sensoren</h2></div>"
+    "<div>"+sensorhtml+"</div>";
+    ;
+
+
+
+void notFound(AsyncWebServerRequest *request) {
+    request->send(404, "text/plain", "Not found");
+}
+void startWebinterface(){
+    Serial.begin(9600);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.printf("WiFi Failed!\n");
+        return;
+    }
+
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        request->send(200, "text/html", index_html);
+    });
+    server.on("/data", HTTP_GET, [](AsyncWebServerRequest *request){
+        String json = "{";
+        for(int i=0; i<sizeof(sensors)/sizeof(*sensors); i++){
+            if (i != 0) json += ", ";
+            json += "\"" + sensors[i].name + "\": \"" + sensors[i].value  + "\"";
+        }
+        json += "}";
+        request->send(200, "text/plain", json);
+    });
+    
+
+    server.onNotFound(notFound);
+
+    server.begin();
+}
